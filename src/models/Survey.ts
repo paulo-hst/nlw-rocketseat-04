14326134f684f4817253ebf542c9a6985033a99f
Paulo <hst.paulo@gmail.com>
2021-02-24 21:50:10 -0300

    dia 03

---
+++ src/models/Survey.ts
@@ -0,0 +1,25 @@
+import { v4 as uuid } from 'uuid'
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+
+@Entity("surveys")
+class Survey {    
+    @PrimaryColumn()
+    readonly id: string;
+    
+    @Column()
+    title: string;
+
+    @Column()
+    description: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    constructor(){
+        if(!this.id){
+            this.id = uuid()
+        }
+    }
+}
+
+export { Survey }
